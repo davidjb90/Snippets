baa84940590f5f9f849b1b78ad0104bff76282c5
davidjb90 <buchanan.david12@gmail.com>
2016-05-25 19:24:58 +0000

    Version with final specified functionality

--- snippets.py
+++ snippets.py
@@ -7,7 +7,7 @@ logging.debug("Connecting to PostgreSQL")
 connection = psycopg2.connect(database="snippets")
 logging.debug("Database connection established.")
 
-def put(name, snippet):
+def put(name, snippet, hide, unhide):
     """
     Store a snippet with an associated name.
 
@@ -17,12 +17,12 @@ def put(name, snippet):
     with connection, connection.cursor() as cursor:
         command = "Insert into snippets values (%s, %s)"
         try:
-            command = "insert into snippets values (%s, %s)"
-            cursor.execute(command, (name, snippet))
+            command = "insert into snippets values (%s, %s, %s)"
+            cursor.execute(command, (name, snippet, hide))
         except psycopg2.IntegrityError as e:
             connection.rollback()
-            command = "update snippets set message=%s where keyword=%s"
-            cursor.execute(command, (snippet, name))
+            command = "update snippets set message=%s, hidden=%s where keyword=%s"
+            cursor.execute(command, (snippet, hide, name))
     connection.commit()
     logging.debug("Snippet stored successfully.")
     return name, snippet
@@ -36,16 +36,28 @@ def get(name):
     Returns the snippet.
     """
     with connection, connection.cursor() as cursor:
-        cursor.execute("select message from snippets where keyword=%s", (name,))
+        command = "select message from snippets where keyword=%s"
+        cursor.execute(command, (name,))
         row = cursor.fetchone()
     logging.error("Snippet retrieved.")
     return row
-
+    
+def search(string):
+    """Find a string in snippets
+    """
+    with connection, connection.cursor() as cursor:
+        command = "Select keyword from snippets where keyword like '%{}%' and not hidden".format(string)
+        cursor.execute(command)
+        c = cursor.fetchall()
+    connection.commit()
+    logging.error("Strings found")
+    return c
+    
 def catalog():
     """Retrieves list of tuples of all the keywords.
     """
     with connection, connection.cursor() as cursor:
-        cursor.execute("select * from snippets order by keyword")
+        cursor.execute("select keyword, message from snippets where not hidden order by keyword") 
         d = cursor.fetchall()
     connection.commit()
     logging.error("Keyword catalog retrieved")
@@ -63,14 +75,22 @@ def main():
     put_parser = subparsers.add_parser("put", help="Store a snippet")
     put_parser.add_argument("name", help="Name of the snippet")
     put_parser.add_argument("snippet", help="Snippet text")
+    put_parser.add_argument("--hide", help="Flags snippet as hidden", action='store_true', default=False)
+    put_parser.add_argument("--unhide", help="Removes hidden flag on snippet", action='store_false', default=False)
     
     # Subparser for the get command
     logging.debug("Constructing get subparser")
     get_parser = subparsers.add_parser("get", help="Retrieve a snippet")
     get_parser.add_argument("name", help="Name of the snippet")
     
+    # Subparser for the catalog command
     logging.debug("Constructing catalog subparser")
     catalog_parser = subparsers.add_parser("catalog", help="Look up snippets by keyword")
+    
+    #Subparser for the search command
+    logging.debug("Constructing catalog subparser")
+    search_parser = subparsers.add_parser("search", help="Find string in snippets")
+    search_parser.add_argument("string", help="String searched")
     
     arguments = parser.parse_args()
     
@@ -88,8 +108,12 @@ def main():
         else:
             print("Retrieved snippet: {!r}".format(snippet))
     elif command == "catalog":
-        snippet = catalog(**arguments)
+        snippet = catalog()
         print("Keywords: {!r}".format(snippet))
+    elif command == "search":
+        snippet = search(**arguments)
+        print("Snippets with string: {!r}".format(snippet))
         
 if __name__ == "__main__":
     main()
+    
